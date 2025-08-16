49c0e54736b15aee8566235464d2d26474fb0337
tomerhanochi <git@tomerhanochi.com>
2025-08-16 11:47:35 +0300

    fix: Use external script because the ubuntu-24.04 runner's buildah doesn't support inline here-doc documents.

---
+++ image/setup.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+set -euxo pipefail;
+
+###############
+#---- K3S ----#
+###############
+# k3s is a multicall binary, and can function as multiple tools depending on the name its called with (similar to busybox).
+for tool in kubectl crictl ctr; do
+  ln -sf /usr/local/bin/k3s "/usr/local/bin/${tool}";
+done
+
+####################
+#---- PACKAGES ----#
+####################
+dnf \
+  --repofrompath=k3s-selinux,https://rpm.rancher.io/k3s/stable/common/centos/9/noarch \
+  --setopt=k3s-selinux.gpgcheck=1 \
+  --setopt=k3s-selinux.gpgkey=https://rpm.rancher.io/public.key \
+  --assumeyes \
+  --disablerepo='*' \
+  --enablerepo='fedora,updates,k3s-selinux' \
+  install NetworkManager openssh-server firewalld k3s-selinux;
+
+####################
+#---- FIREWALL ----#
+####################
+# Enable access to the API server
+firewall-offline-cmd --add-service=kube-apiserver;
+# Enable internal communication between pods
+firewall-offline-cmd --zone=trusted --add-source=10.42.0.0/16;
+# Enable internal communication between services
+firewall-offline-cmd --zone=trusted --add-source=10.43.0.0/16;
+
+###################
+#---- SYSTEMD ----#
+###################
+systemctl enable var-home.mount k3s.service
